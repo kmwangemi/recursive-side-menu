210e404cbec6c35219b8ab8906900ac23c5d2c8f
kmwangemi <mwangemik@gmail.com>
2021-11-25 13:05:49 +0300

    add security component

--- src/components/security/index.js
+++ src/components/security/index.js
@@ -1,9 +1,15 @@
+import { Routes, Route, useRoutes } from 'react-router-dom';
 // import style from './home.module.css';
 
 const Security = () => {
+   let { path } = useRoutes();
+   console.log('security', { path });
    return (
       <div>
-         Security Page
+         <Routes>
+            <Route path={`${path}/credentials`}>Credentials</Route>
+            <Route path={`${path}/2fa`}>Two factor authentication</Route>
+         </Routes>
       </div>
    )
 }
